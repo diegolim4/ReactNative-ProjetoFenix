d5985768bfa664c83d10ed6a0b70c88dc1ba92ac
Suelen Almeida <lima.j.diego@gmail.com>
2021-05-14 13:57:22 -0300

    Hello Fenix

--- App.js
+++ App.js
@@ -1,21 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, SafeAreaView , View, TextInput, StatusBar, TouchableOpacity } from 'react-native';
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [arquivo, setArquivo] = useState('') 
+  
+  
+  return (    
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor='#35AAFF' barStyle='dark-content' />    
+      <View style={styles.txtBox}>
+         <Text style={styles.txt}>Hello Fenix</Text>        
+      </View>
+      <View > 
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btn_txt}>###btn</Text>            
+        </TouchableOpacity>
+      </View>         
+    </SafeAreaView>
+  );    
+    
+}  
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#8B0000',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
+              
   },
+
+  txt:{
+    fontSize: 25,
+    color: '#E8E8E8',
+    marginBottom: 550,
+    margin: 5,           
+  },
+  
+  btn:{  
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor:'#35AAFF',
+    padding:15,
+    borderRadius:7,
+    width:'100%',
+    padding:10,
+    height: 35,
+    marginBottom: 550,
+  }, 
+  
+  btn_txt:{
+    color: '#F5F5DC',
+    fontSize: 20 ,
+  }
 });
